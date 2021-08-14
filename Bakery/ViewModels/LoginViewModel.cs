0ac33d234151472474b6605cdb983ec0aa3403d0
Simple Dimple John <simpledimplejohn@gmail.com>
2021-08-14 11:33:35 -0700

    add files to viewmodels

---
+++ Bakery/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+namespace Bakery.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+// these strings are used in the form in the Account/Login.cshtml
